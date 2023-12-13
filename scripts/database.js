64e69dfdf48d8e68060fcad97fea99f1b8bd54e7
Ruth Kirwa <ruthie.kirwa@gmail.com>
2023-12-13 16:25:26 -0600

    add files

---
+++ scripts/database.js
@@ -0,0 +1,18 @@
+/*
+ *  To get you started, here's some properties of Bart.
+ *  You need to add more properties to complete his
+ *  representation as an object. Then add all the other
+ *  fish to the collection.
+ */
+const database = {
+    fish: [
+        {
+            name: "Bart",
+            food: "crustaceans",
+            size: 3
+        }
+    ]
+}
+export const getFish = () => {
+    return database.fish.map(fish => ({...fish}))
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 const theExistingMainElement = document.querySelector("#container")
-theExistingMainElement.innerHTML = theWholeEnchilada
+import { getFish } from './database.js'
+
+const allFish = getFish()
+
+for (const fish of allFish) {
+    console.log(fish)
+}
