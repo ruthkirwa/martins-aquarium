62673adf2270b8baa20c95eecac23ca06f57593b
Ruth Kirwa <ruthie.kirwa@gmail.com>
2023-12-14 10:02:59 -0600

    fishlist.js to repo

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,17 @@ const allFish = getFish()
 for (const fish of allFish) {
     console.log(fish)
 }
+// Import the FishList function from the correct module
+import {} from ''
+
+/*
+    What is the CSS selector for the element where you
+    want to display the fish?
+
+    Use . for elements with a "class" attribute
+    Use # for elements with an "id" attribute
+ */
+const parentHTMLElement = document.querySelector("insert selector here")
+
+
+parentHTMLElement.innerHTML = FishList()
